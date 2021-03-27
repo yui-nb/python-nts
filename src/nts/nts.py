ad297f1c90a9107f78f1d5965b55ac136e151e45
Yui En <yui.github@mailbox.org>
2021-03-27 02:08:19 +0100

    feat: add filter getter/setter

--- src/nts/nts.py
+++ src/nts/nts.py
@@ -1,6 +1,9 @@
 from enum import Enum
 import rtmidi
 
+FILTER_TYPE_CC = 42
+FILTER_CUTOFF_CC = 43
+FILTER_RESONANCE_CC = 44
 
 class FilterType(Enum):
     LP2 = 0
@@ -16,7 +19,11 @@ class NTS:
     def __init__(self, channel:int =0):
         self.__midi_out = rtmidi.MidiOut()
         self.__midi_out.open_port(self.get_port_index())
-        self.channel = channel
+        self.channel: int = channel
+        
+        self._filter_type: FilterType = FilterType.OFF
+        self._filter_cutoff: int = 0
+        self._filter_resonance: int = 0
 
     def get_port_index(self) -> int:
         for index, port in enumerate(self.__midi_out.get_ports()):
@@ -26,25 +33,28 @@ class NTS:
 
     @property
     def filter_type(self) -> FilterType:
-        pass
+        return self._filter_type
 
     @filter_type.setter
     def filter_type(self, value: int):
-        pass
+        self._filter_type = FilterType(value)
+        self.__midi_out.send_message([0xB0, FILTER_TYPE_CC, self._filter_type.value])
 
     @property
     def filter_cutoff(self) -> int:
-        pass
+        return self._filter_cutoff
 
     @filter_cutoff.setter
     def filter_cutoff(self, value: int):
-        pass
+        self._filter_cutoff = value
+        self.__midi_out.send_message([0xB0, FILTER_CUTOFF_CC, self._filter_cutoff])
 
     @property
     def filter_resonance(self) -> int:
-        pass
+        return self._filter_resonance
 
     @filter_resonance.setter
     def filter_resonance(self, value: int):
-        pass
+        self._filter_resonance = value
+        self.__midi_out.send_message([0xB0, FILTER_RESONANCE_CC, self._filter_resonance])
 
